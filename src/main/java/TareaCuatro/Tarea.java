b70190ac9540d52a419ebcd6e0c42e2925d71d48
mauromg18 <mauro.smg.1801@gmail.com>
2023-12-17 23:42:38 -0300

    empezando

---
+++ src/main/java/TareaCuatro/Tarea.java
@@ -0,0 +1,26 @@
+package TareaCuatro;
+import java.io.Serializable;
+
+public class Tarea implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String descripcion;
+    private EstadoTarea estado;
+
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.estado = EstadoTarea.POR_HACER;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public EstadoTarea getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoTarea estado) {
+        this.estado = estado;
+    }
+}
